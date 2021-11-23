f2159d3c0f9a61ea2933d28ee3691ea11d99f951
Rasel Miah <rasel6041@gmail.com>
2021-11-23 02:38:42 +0100

    project 5 pass

--- JS-Learning/function.js
+++ JS-Learning/function.js
@@ -1,11 +1,40 @@
+//
+
 function print(arg){
-    console.log(arg)
-}
-function add(num1, num2){
-    console.log(num1+num2);
+    //console.log(arg)
 }
 
-print("tanim");
-add(7.02, 15.97);
+
+function functionWithArgs(param1, param2){
+    result=param1+param2;
+}
+functionWithArgs("test", " func");
+//console.log(result);
+
+//Return a Value from a Function with Return
+function timesFive(num){
+    return num*5;
+  }
+  let ans=timesFive(10);
+  //console.log(ans)
 
 
+
+  //Global Scope and Functions
+let myGlobal=10;
+function fun1() {
+      oopsGlobal=5;
+}
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+fun1()
+fun2()
