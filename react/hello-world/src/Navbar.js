24f9ced6a164021789917960dd5a6041412c5720
Rasel Miah <rasel6041@gmail.com>
2021-12-24 23:05:24 +0100

    react

---
+++ react/hello-world/src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1> Rasel's Blog</h1>
+            <div>
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+
+ );
+}
+ 
+export default Navbar;
