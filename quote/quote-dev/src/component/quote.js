b959f7e3c6e5466b230dcce0f55fb050bd9763ed
Rasel Miah <rasel6041@gmail.com>
2022-01-04 04:54:56 +0100

    fetch data from json api

--- quote/quote-dev/src/component/quote.js
+++ quote/quote-dev/src/component/quote.js
@@ -1,40 +1,40 @@
 import { useState, useEffect} from 'react';
 
 const Quote = () => {
-    const [quote, setQuote] = useState('Hello Quote Matchine, This is from react');
+    const [quote, setQuote] = useState('Never compare myself to somebody else, I compare my today with yesterdy');
     const [autname, setAutname]=useState('Rasel');
     const [quotebox, setBoxcolor]=useState("quote-box");
     const [bodycolor, setBodycolor]=useState("body");
-    const [randomNumber, setRandomNumber]=useState('0')
+    const [randomNumber, setRandomNumber]=useState('0');
 
-    let quoteLink="https://github.com/raselrzl/raselrzl.github.io/blob/main/API/quotes.json"
+    const [quotesArray, setQuotesArray]=useState(null);
 
-   // const [apiData, setApiData]=useState([])
-
-/*     const apiGet=()=>{
-        fetch("https://raselrzl.github.io/API/quotes.json")
-        .then((response)=>response.json())
-        .then((json)=>{
-            console.log(json);
-            setApiData(json);
-        });
-    };
-
-    useEffect(()=>{
-        apiGet();
-    },[]) */
+    let quoteLink="https://raselrzl.github.io/API/quotes.json";
     
+
+    const fetchQuotes=async(url)=>{
+        const res=await fetch(url);
+        const getJSON= await res.json();
+        setQuotesArray(getJSON.quotes);
+        console.log(getJSON);
+    }
+    useEffect(()=>{
+        fetchQuotes(quoteLink);
+    },[quoteLink]);
+
+    
+
+   
     
     const handleClick=()=>{       
        setBoxcolor("box-color");
        setBodycolor("body2");
        
-       let quoteArray=[{quote:"Life isn’t about getting and having, it’s about giving and being.",author:"Kevin Kruse"},{quote:"Whatever the mind of man can conceive and believe, it can achieve.",author:"Napoleon Hill"},{quote:"Strive not to be a success, but rather to be of value.",author:"Albert Einstein"}];
-       let randomValue=Math.floor(Math.random()*quoteArray.length);
+       let randomValue=Math.floor(Math.random()*quotesArray.length);
        setRandomNumber(randomValue);
        
-        setQuote(quoteArray[randomValue].quote);
-        setAutname(quoteArray[randomValue].author);
+        setQuote(quotesArray[randomValue].quote);
+        setAutname(quotesArray[randomValue].author);
        
     }
 
@@ -45,14 +45,13 @@ const Quote = () => {
                    
         
             <div>
-                <h1 id="text"><span><i class="bi bi-chat-square-quote-fill"></i></span>  {quote}</h1>
-                <h2>{randomNumber}</h2>
+                <h3 id="text"><span><i class="bi bi-chat-square-quote-fill"></i></span>  {quote}</h3>
                 <p id="author"><i class="bi bi-pen-fill"></i> {autname}</p>
             </div>
             <div className="flex-container">
             <div className="buttons" >
                  <div class="logo"><a><i class="bi bi-facebook"></i></a></div>
-                 <div class="logo"><a id="tweet-quote"><i class="bi bi-twitter"></i></a></div>                   
+                 <div class="logo"><a id="tweet-quote"><i class="bi bi-twitter"></i></a></div>               
             </div>
             <div className="buttons" id="next-quote-button"><button id="new-quote" onClick={()=>handleClick()}>Next Quote</button></div>
             </div>
