e30146da8aef8957063da765424c8c35bc5b6ab5
Rasel Miah <rasel6041@gmail.com>
2022-01-04 09:52:05 +0100

    quotes

--- quote/quote-dev/src/component/quote.js
+++ quote/quote-dev/src/component/quote.js
@@ -1,11 +1,11 @@
 import { useState, useEffect} from 'react';
 
 const Quote = () => {
-    const [quote, setQuote] = useState('Never compare myself to somebody else, I compare my today with yesterdy');
-    const [autname, setAutname]=useState('Rasel');
-    const [quotebox, setBoxcolor]=useState("quote-box");
+    const [quote, setQuote] = useState('I never compare to others neither I did, just compare to me, want to make today better then of mine yesterday');
+    const [autname, setAutname]=useState('Dr. Zeras');
+   
     const [bodycolor, setBodycolor]=useState("body");
-    const [randomNumber, setRandomNumber]=useState('0');
+    
 
     const [quotesArray, setQuotesArray]=useState(null);
 
@@ -23,40 +23,39 @@ const Quote = () => {
     },[quoteLink]);
 
     
-
+    
    
     
     const handleClick=()=>{       
-       setBoxcolor("box-color");
-       setBodycolor("body2");
-       
-       let randomValue=Math.floor(Math.random()*quotesArray.length);
-       setRandomNumber(randomValue);
+      
+       setBodycolor("body2");       
+       let randomValue=Math.floor(Math.random()*quotesArray.length);     
        
         setQuote(quotesArray[randomValue].quote);
         setAutname(quotesArray[randomValue].author);
+        
+        let colorArr=['body2', 'body3', 'body4', 'body5', 'body6', 'body7', 'body8', 'body9', 'body10']
+        let randomColor=Math.floor(Math.random()*colorArr.length);
+        setBodycolor(colorArr[randomColor]);
+        
        
     }
 
     return (      
-        <div id={bodycolor}> 
-        
-        <div id={quotebox}>  
-                   
-        
-            <div>
+        <div id={bodycolor}>
+            <div  id="quote-box">                    
                 <h3 id="text"><span><i class="bi bi-chat-square-quote-fill"></i></span>  {quote}</h3>
                 <p id="author"><i class="bi bi-pen-fill"></i> {autname}</p>
-            </div>
-            <div className="flex-container">
-            <div className="buttons" >
-                 <div class="logo"><a><i class="bi bi-facebook"></i></a></div>
-                 <div class="logo"><a id="tweet-quote"><i class="bi bi-twitter"></i></a></div>               
-            </div>
-            <div className="buttons" id="next-quote-button"><button id="new-quote" onClick={()=>handleClick()}>Next Quote</button></div>
-            </div>
-        </div>
-       
+                    
+                <div className="flex-container">
+                    <div className="buttons" >
+                        <div class="logo"><a target="_blank" href={encodeURI(`https://facebook.com?text=${quote}-${autname}`)}><i class="bi bi-facebook"></i></a></div>
+                        <div class="logo"><a target="_blank" id="tweet-quote" href={encodeURI(`https://twitter.com/intent/tweet?text=${quote}-${autname}`)}><i class="bi bi-twitter"></i></a></div>
+                    </div>
+
+                    <div className="buttons" id="next-quote-button"><button id="new-quote" onClick={()=>handleClick()}>Next Quote</button></div>
+                </div>
+            </div>       
         </div>
         
      );
