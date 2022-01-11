d4b3cae3dcf90fa84fda093d87981685c6a91317
Rasel Miah <rasel6041@gmail.com>
2022-01-11 02:19:35 +0100

    markdown lib added

--- Markdown-Previewer/markdown-previewer-dev/src/MarkdownPreviewer.js
+++ Markdown-Previewer/markdown-previewer-dev/src/MarkdownPreviewer.js
@@ -1,21 +1,16 @@
 import React, {useState} from 'react'
-
+import Markdown from 'react-markdown';
 
 function MarkdownPreviewer() {
     const [inputData, setInputData]=useState(null)
 
-
-    function getData(val){
-        setInputData(val.target.value);
-    }
-
     return (
         
         <div id="input-area">
-            <div id="editor-header">Editor</div>
-            <textarea id="editor" onChange={getData}></textarea>
-            <div id="previewer-header">Previewer</div>
-            <div id="preview"> {inputData}</div>
+            <div id="editor-header"><p>Editor</p></div>
+            <textarea id="editor" value={inputData} onChange={e=>setInputData(e.target.value)}></textarea>
+            <div id="previewer-header"><p>Previewer</p></div>
+            <div id="preview"><Markdown>{inputData}</Markdown></div>         
         </div>
 
 
