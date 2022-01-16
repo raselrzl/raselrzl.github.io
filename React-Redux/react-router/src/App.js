fbf237bf95e5d41e1eff030c0c2442fb864ad176
Rasel Miah <rasel6041@gmail.com>
2022-01-16 21:45:06 +0100

    request to server

--- React-Redux/react-router/src/App.js
+++ React-Redux/react-router/src/App.js
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div className="App">
-        <h1>React router</h1>
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import Navbar from './component/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './component/Home'
+import Contact from './component/Contact'
+import About from './component/About'
 
+class App extends Component {
+  render() {
+    return (       
+      <Router>
+       <Navbar/>
+       <Routes>          
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+       </Routes>
+      </Router>
+     
+    );
+  }
+}
 export default App;
