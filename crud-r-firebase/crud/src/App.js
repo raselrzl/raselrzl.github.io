9b43dd9e90b4ea523bf05e87124f0b8739ec61e2
Rasel Miah <rasel6041@gmail.com>
2022-01-17 20:32:39 +0100

    crud done

--- crud-r-firebase/crud/src/App.js
+++ crud-r-firebase/crud/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import { useState, useEffect } from 'react'
 import {db} from './firebase-config'
-import { collection, getDocs } from 'firebase/firestore' 
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from 'firebase/firestore' 
 
 function App() {
+  const [newName, setNewName]=useState("")
+  const [newAge, setNewAge]=useState(0)
+
   const[users, setUsers] = useState([]);
   const userCollectionRef = collection(db, 'users')
 
@@ -18,17 +21,39 @@ function App() {
   }, [ ])
 
   
+  const createUser=async()=>{
+      await addDoc(userCollectionRef, { name: newName, age: Number(newAge)})
+  }
+
+  const updateUser=async(id,age)=>{
+      const userDoc=doc(db, "users", id)
+      const newFields={age:age+1}
+      await updateDoc(userDoc, newFields)
+  }
+
+  const deleteUser=async(id)=>{
+      const userDoc=doc(db, 'users', id)
+      await deleteDoc(userDoc)
+  }
+
   return (
     <div className="App">
-     
+       <input placeholder='Name...'  onChange={ (e)=>{setNewName(e.target.value)}}></input>
+       <input placeholder='Age...' type="number" onChange={ (e)=>{setNewAge(e.target.value)}}></input>
+       <button onClick={createUser}>Create User</button>
+
       {users.map((user)=>{
         return(
           <div>
             <h1> Name:{user.name}</h1>
             <p>Age:{user.age}</p>
+            <button onClick={()=>updateUser(user.id, user.age)}>Increase Age</button>
+            <button onClick={()=>{deleteUser(user.id)}}>Delete User</button>
           </div>
         )
       })}
+
+
     </div>
   );
 }
