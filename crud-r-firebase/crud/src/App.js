e30d31398246806e83a783fa1bc6ccd2f6c1aebb
Rasel Miah <rasel6041@gmail.com>
2022-01-17 19:55:30 +0100

    get data from db

--- crud-r-firebase/crud/src/App.js
+++ crud-r-firebase/crud/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+import {db} from './firebase-config'
+import { collection, getDocs } from 'firebase/firestore' 
 
 function App() {
+  const[users, setUsers] = useState([]);
+  const userCollectionRef = collection(db, 'users')
+
+
+  useEffect(() => {
+    const getUsers= async()=>{
+         const data=await getDocs(userCollectionRef);
+         console.log(data)
+         setUsers(data.docs.map((doc)=>({...doc.data(), id:doc.id})));
+    }
+   getUsers()
+  }, [ ])
+
+  
   return (
     <div className="App">
-      Hello Firebase
+     
+      {users.map((user)=>{
+        return(
+          <div>
+            <h1> Name:{user.name}</h1>
+            <p>Age:{user.age}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
